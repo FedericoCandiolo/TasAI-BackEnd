14ed007cafa9c4189a8d8a51d7c6ed281a250da1
Leandro Oviedo <38381583+OviedoLeandro@users.noreply.github.com>
2023-09-10 17:05:36 -0300

    REST Framework added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    id = models.Model.pk
+    nombre_usuario = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    nombre_publico = models.CharField(max_length=100)
