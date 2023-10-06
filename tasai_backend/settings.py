a78600689f3aac047653fe857239fdbc91383815
Leandro Oviedo <38381583+OviedoLeandro@users.noreply.github.com>
2023-10-06 20:23:03 -0300

    Added CORS config on settings.py

--- tasai_backend/settings.py
+++ tasai_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
     'drf_yasg',
+    'corsheaders',
 
 ]
 
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'tasai_backend.urls'
 
